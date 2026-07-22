eba71f061a000e9e5c562ad1987556fe402d2193
Sneha Bhingare <snehabhingare9011@gmail.com>
2026-07-23 01:44:13 +0530

    tag component created successfully

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import Random from './components/Random';
 import Tag from './components/Tag';
-import { useState } from 'react';
 
 function App() {
-
   return (
-    <div className="App">
-      <div>
-        <h1>Random GIFS</h1>
+    <div className="min-h-screen bg-blue-200 bg-[radial-gradient(white_2px,transparent_2px)] [background-size:22px_22px] flex flex-col items-center py-8">
+
+      <div className="w-11/12 max-w-5xl bg-white rounded-lg shadow-md py-4 mb-10">
+        <h1 className="text-4xl font-bold uppercase text-center tracking-wide">
+          Random GIFS
+        </h1>
       </div>
+
       <Random />
-      <Tag/>
+
+      <Tag />
+
     </div>
   );
 }
--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import Spinner from './Spinner';
 
 const Tag = () => {
+    const [loading,setLoading]=useState(false);
+    const[tag,setTag]=useState("car");
+
+    const [gif,setGif]=useState('');
+    const API_KEY=process.env.REACT_APP_GIPHY_API_KEY; 
+
+    console.log("api key",API_KEY);
+
+    async function fetchData(){
+
+       setLoading(true);
+        const url=`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}$tag=${tag}`;
+
+        const responce=await axios.get(url);
+        let img_url=responce.data.data.images.downsized_large.url;
+        console.log("image_url",img_url);
+        setGif(img_url)
+       setLoading(false);
+
+    }
+    useEffect(()=>{
+        fetchData();
+
+    },[]);
+
+    function clickHandler(){
+        fetchData()
+
+    }
   return (
     <div>
+        <h3>
+            Random {tag} GIF
+        </h3>
+        {loading?<Spinner/>:<img src={gif}></img>}
         
+        <input type="text" onChange={(event)=>setTag(event.target.value)} value={tag}></input>
+        <button  onClick={clickHandler}>Generate</button>
     </div>
   )
 }
